d3d1d3e4e55c3723db3d1d5af0478c990592f005
Don <evidence@384.jp>
2023-03-01 08:08:49 +0900

    型定義ファイルを分離

--- src/angolmois-web/src/main.ts
+++ src/angolmois-web/src/main.ts
@@ -9,45 +9,7 @@ import {
 	signEvent
 } from 'nostr-tools';
 import 'websocket-polyfill';
-
-// https://gist.github.com/syusui-s/cd5482ddfc83792b54a756759acbda55
-// This code is Public domain (Creative Commons Zero CC0-1.0)
-// https://creativecommons.org/publicdomain/zero/1.0/deed.ja
-
-// from nostr-tools (Public domains)
-export type NostrEvent = {
-	id?: string;
-	kind: number;
-	tags: string[][];
-	pubkey: string;
-	content: string;
-	created_at: number;
-};
-
-type NostrAPI = {
-	/** returns a public key as hex */
-	getPublicKey(): Promise<string>;
-	/** takes an event object, adds `id`, `pubkey` and `sig` and returns it */
-	signEvent(event: Event): Promise<NostrEvent>;
-
-	// Optional
-
-	/** returns a basic map of relay urls to relay policies */
-	getRelays?(): Promise<{ [url: string]: { read: boolean; write: boolean } }>;
-
-	/** NIP-04: Encrypted Direct Messages */
-	nip04: {
-		/** returns ciphertext and iv as specified in nip-04 */
-		encrypt(pubkey: string, plaintext: string): Promise<string>;
-		/** takes ciphertext and iv as specified in nip-04 */
-		decrypt(pubkey: string, ciphertext: string): Promise<string>;
-	};
-};
-
-interface Window {
-	nostr?: NostrAPI;
-}
-declare var window: Window;
+import { window } from './@types/nostr';
 
 (function (){
 	const defaultRelays = [
